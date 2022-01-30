146a53c892b83016fc98988cbf0bd31bca31fbd1
(nithinmkp) <write2nithinm@gmail.com>
2022-01-30 15:28:49 +0530

    after visit 2, round 70

--- code.R
+++ code.R
@@ -1,30 +1,54 @@
 #packages
 source("Functions.R")
-packages<-c("tidyverse","readxl","haven","rio","here","nombre","fs",
-            "openxlsx","writexl","powerjoin","data.table")
+packages<-c("tidyverse","readxl","haven","rio","here","nombre","fs","glue",
+            "openxlsx","writexl","powerjoin","data.table","furrr")
 package_fn(packages)
 
+#Round and Visit Information
+round<-"Round-70"
+visit<-"Visit-2"
+
 #read data
-text_files<-list.files(pattern = "*.TXT",path = paste0(here(),"/Data/Visit-1/"))
-raw_data_files<-paste0(here(),"/Data/Visit-1/",text_files)
-raw_data<-map(raw_data_files,read.delim,header=F,sep="\t") %>% 
-        set_names(paste(nom_ord(1:17),"data",sep = "_"))
+text_files<-list.files(pattern = "*.TXT",path = paste0(here(),"/Data/",
+                                                       round,"/",visit,"/"))
+raw_data_files<- paste0(here(),"/Data/",
+                        round,"/",visit,"/",text_files)
+raw_data<-map(raw_data_files,read.delim,header=F,sep="\t") 
 #raw_data<-raw_data[-1] #comment this line out if you want to use block 1 and block 2 info
 
 
 #layout specifications sheet
 lay_out_excel<-read_excel(path =paste0(here(),
-                                       "/Supporting documents/data_layout33v1.xls" ))
+                                       "/Supporting documents/",
+                                       round,"/","data_layout33v2.xls" ))
 
 #wrangling- layout sheet
 n1<-which(lay_out_excel$`VISIT NUMBER 1 AND 2`=="Item") # identify column name start
 n2<-which(lay_out_excel$`VISIT NUMBER 1 AND 2`=="Blank") #identify the end of column names
 n2<-n2-1 #adjust the blank column
 n3<-n1+1 # skip header row
-slice_num<-map2(n3,n2,seq) %>% 
-        set_names(paste0("slice_cond",1:17))
-#slice_num<-slice_num[-1] #simillarly here, comment out for using block 1 and 2 info
 
+#layout information- based on availability of text-files
+exclude_levels<-NULL
+
+if(round=="Round-70" && visit=="Visit-2"){
+        exclude_levels<-c(2,13)
+}else if(round=="Round-70" && visit=="Visit-1" |round=="Round-77" && visit=="Visit-1" ){
+        exclude_levels<-NULL
+}else{
+        exclude_levels<-c(3,9,15)
+}
+
+levels_label<-cardinal(seq_along(n1))[-exclude_levels]
+
+#rename data files
+raw_data<-raw_data %>% set_names(paste("level",levels_label,sep = "-"))
+
+#Exclude some levels
+slice_num<-map2(n3,n2,seq) %>% 
+        set_names(paste0("slice_cond",seq_along(n2)))
+#slice_num<-slice_num[-1] #similarly here, comment out for using block 1 and 2 info
+slice_num<-slice_num[-exclude_levels]
 info_tbl<-map(slice_num,~lay_out_excel %>% 
                        slice(.x) %>% 
                        select("into"=2,
@@ -45,7 +69,8 @@ data_list<-data_list %>%
 level_1_sep<-map(sep_vec_list[1],~replicate(n=length(raw_data)-1,
                                   .x,simplify = F)) %>% flatten() %>% 
         map(~.x[1:16])
-        
+
+#Exclude level one information        
 all_levels_sep<-sep_vec_list[-1]
 new_sep<-map2(level_1_sep,all_levels_sep,~c(.x,.y))
 
@@ -75,23 +100,24 @@ data_list_common2<-map(data_list_common,~.x %>% select(-c(1,3:13))) %>%
                                                                  as.numeric)))
 
 #Folder for Excel Sheets
-output_folder<-"Excel-Sheets"
+output_folder<-paste0("Excel-Sheets/",round)
 dir_create(output_folder)
 
 #Write into one excel workbook all data
-openxlsx::write.xlsx(data_list,paste0(here(),"/",output_folder,"/all_data.xlsx"))
+openxlsx::write.xlsx(data_list,paste0(here(),"/",output_folder,"/all_data-",
+                                      visit,".xlsx"))
 
 #Write each data into separate excel workbooks- may be for processing in stata/spss
 master_sheets<-"Master-Sheets"
 dir_create(paste0(output_folder,"/",master_sheets))
-walk2(data_list,paste0(here(),"/",output_folder,"/",master_sheets,
-                       "/Level-",1:17,"_full",".xlsx"),
-      ~write.xlsx(x=.x,file = .y))
+iwalk(data_list,
+      ~write.xlsx(x=.x,file = glue(here(),"/",output_folder,"/",master_sheets,"/",
+                                     .y,"_{visit}",".xlsx")))
 
 #Write all data excluding level 1 into separate excel files
 level_data<-paste0(output_folder,"/","Level-Data")
 dir_create(level_data)
-file_list<-paste0(here(),"/",level_data,"/Level-",2:17,".xlsx")
+file_list<-glue(here(),"/",level_data,"/{names(data_list_common2)}_{visit}",".xlsx")
 tibble(x=data_list_common2,file=file_list) %>% pwalk(write.xlsx)
 
 #merging- before creating common-id - no more meaningful
@@ -101,14 +127,18 @@ merge_data<-data_list %>% reduce(full_join,by="Common-ID")
 openxlsx::write.xlsx(merge_data,paste0(here(),"/",output_folder,"/merged.xlsx"))
 
 #Merging Household data- after creation of common id
+individual_level<-paste("level",cardinal(3),sep = "-")
 #level 3 is individual level data- so we leave that out
-merge_raw_data<-data_list_common2[-2]
+merge_raw_data<-data_list_common2 %>% discard(names(data_list_common2)==individual_level)
 rm(raw_data,data_list,data_list_common,new_data_list,data_list_common2) #remove earlier created lists to save memory
 gc() #clean unused memory
 
 #Convert final list of data to data.table
 list_DT<- map(merge_raw_data,setDT,key="common-id")
 
-merged_data_final<-merge_raw_data %>% reduce(power_inner_join,by="common-id")
+#merged_data_final<-merge_raw_data %>% reduce(power_inner_join,by="common-id")
 
+merged_DT<-as.data.table(list_DT)
 
+#Write Merged Data
+write.xlsx(merged_DT,glue("merged_data","-{round}-","{visit}",".xlsx"))
